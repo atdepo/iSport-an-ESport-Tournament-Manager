5b85c4c225fa47309713a4b730877d5e1e515847
ndtefu <antonior1999a@gmail.com>
2020-08-10 05:32:40 +0200

    Update insGiocatori.js

--- WebContent/Script/insGiocatori.js
+++ WebContent/Script/insGiocatori.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 	var div=$ (".inseriamo");
-	  var numeroGiocatori= $("#giocatori")
-	  div.append("<h3>"+numeroGiocatori+"</h3>");
+	  var numeroGiocatori= $("#giocatori").val();
+	  
 	  for (let i = 0; i < numeroGiocatori; i++) {
-		  div.append("<h3>"+numeroGiocatori+"</h3>");
+		  div.append("<div class='giocatore'>" +
+		  		"<input type='text' placeholder='nickname' id='nickname'>" +
+		  		"<input type='text' placeholder='nome' id='nome'>" +
+		  		"<input type='text' placeholder='cognome' id='cognome'>" +
+		  		"<input type='date' id='data'" +
+		  		"<h3>il nome della squadra lo prendiamo dalla sessione</h3></div");
 	  }
 })
